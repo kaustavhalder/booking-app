8ee07eed7fdcfa2db54909361371d833c213f5cc
kaustavhalder <khalder.08@gmail.com>
2023-10-08 12:31:44 -0600

    assigned non negative stuff

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import "fmt"
 func main() {
 	var confName = "Go Conference"
 	const confTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
+
 	fmt.Println("Welcome to", confName, "Booking Application")
 	fmt.Println("Get tickets to attend the conference")
 	fmt.Println("Total Tickets", confTickets, "Still Available", remainingTickets)
+	fmt.Printf("type of confTickets %T and confName %T\n", confTickets, confName)
 
 }
