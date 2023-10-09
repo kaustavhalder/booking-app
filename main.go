59109d454432fa05f928a16e1f3922617cdf3703
kaustavhalder <khalder.08@gmail.com>
2023-10-08 19:20:58 -0600

    inifinte loop

--- main.go
+++ main.go
@@ -6,23 +6,29 @@ func main() {
 	var confName = "Go Conference"
 	const confTickets = 50
 	var remainingTickets uint = 50
+	var bookings []string
 
 	fmt.Println("Welcome to", confName, "Booking Application")
 	fmt.Println("Get tickets to attend the conference")
 	fmt.Println("Total Tickets", confTickets, "Still Available", remainingTickets)
 
-	var userName string
-	var userTickets int
-	// Get user input
-	fmt.Println("Enter your name")
-	fmt.Scan(&userName)
+	for {
+		var userName string
+		var userTickets int
+		// Get user input
+		fmt.Println("Enter your name")
+		fmt.Scan(&userName)
 
-	fmt.Println("Enter number of tickets")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter number of tickets")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = confTickets - uint(userTickets)
+		remainingTickets = remainingTickets - uint(userTickets)
+		bookings = append(bookings, userName)
 
-	fmt.Printf("User %v bought %v tickets\n", userName, userTickets)
-	fmt.Printf("Remaining Tickets in the conference %v\n", remainingTickets)
+		fmt.Printf("User %v bought %v tickets\n", userName, userTickets)
+		fmt.Printf("Remaining Tickets in the conference %v\n", remainingTickets)
+		fmt.Printf("Total Bookings %v\n", bookings)
+
+	}
 
 }
