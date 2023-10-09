a4f13b6f4b91827cd26d18bb5f7d9d1f2ce57854
kaustavhalder <khalder.08@gmail.com>
2023-10-08 21:41:20 -0600

    refactor

--- main.go
+++ main.go
@@ -57,11 +57,6 @@ func main() {
 	}
 }
 
-func greetUser() {
-	fmt.Println("Welcome to", confName, "Booking Application")
-	fmt.Println("Get tickets to attend the conference")
-	fmt.Println("Total Tickets", confTickets, "Still Available", remainingTickets)
-}
 
 func printFirstName(bookings []string) {
 	firstNames := []string{}
