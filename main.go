07fb72586b6a1734a54f6cd1ab323ddbe956302d
kaustavhalder <khalder.08@gmail.com>
2023-10-08 20:31:35 -0600

    refactor

--- main.go
+++ main.go
@@ -29,20 +29,30 @@ func main() {
 		fmt.Println("Enter number of tickets")
 		fmt.Scan(&userTickets)
 
-		// Perform operations
-		remainingTickets = remainingTickets - uint(userTickets)
-		bookings = append(bookings, firstName+" "+lastName)
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isUserTickets := userTickets > 0 && userTickets < 50
 
-		fmt.Printf("User %v bought %v tickets\n", firstName+" "+lastName, userTickets)
-		fmt.Printf("Remaining Tickets in the conference %v\n", remainingTickets)
+		if isValidName && isUserTickets {
+			remainingTickets = remainingTickets - uint(userTickets)
+			bookings = append(bookings, firstName+" "+lastName)
 
-		firstNames := []string{}
-		for _, val := range bookings{
-			var name = strings.Fields(val)
-			firstNames = append(firstNames, name[0])
+			fmt.Printf("User %v bought %v tickets\n", firstName+" "+lastName, userTickets)
+			fmt.Printf("Remaining Tickets in the conference %v\n", remainingTickets)
+
+			firstNames := []string{}
+			for _, val := range bookings {
+				var name = strings.Fields(val)
+				firstNames = append(firstNames, name[0])
+			}
+			fmt.Printf("Bookings made by %v\n", firstNames)
+			if remainingTickets == 0 {
+				fmt.Println("Tickets have been sold out")
+				break
+			}
+
+		} else {
+			// fmt.Printf("we only have %v tickets available, you cant book %v tickets\n", remainingTickets, userTickets)
+			fmt.Println("User data input is invalid, try again")
 		}
-		fmt.Printf("Bookings made by %v\n", firstNames)
-
 	}
-
 }
